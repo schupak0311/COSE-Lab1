81957728d3a8e9a3b085dbda113f42e204e10fc6
Alex_Pivnenko <alex.pivnenko.99@gmail.com>
2018-05-14 11:33:23 +0300

    end

--- build/lib/sport_analyzer/MatchResults.py
+++
@@ -1,7 +0,0 @@
-class MatchResults:
-    def __init__(self, url, hometeam, awayteam, homescore, awayscore):
-        self.site_url = url
-        self.homeTeam = hometeam
-        self.awayTeam = awayteam
-        self.homeScore = homescore
-        self.awayScore = awayscore
--- build/lib/sport_analyzer/__init__.py
+++
@@ -1,1 +0,0 @@
-from Sitereader import createMatchResult
